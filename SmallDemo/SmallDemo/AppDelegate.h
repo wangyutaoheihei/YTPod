148027831e1fa784ecf382a46126b10c8b94aae0
qiancheng <qianc@zcitc.com>
2017-01-17 15:37:19 +0800

    no message

---
+++ SmallDemo/SmallDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmallDemo
+//
+//  Created by ZhiCheng on 2017/1/17.
+//  Copyright © 2017年 玉涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmallDemo/SmallDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmallDemo
+//
+//  Created by ZhiCheng on 2017/1/17.
+//  Copyright © 2017年 玉涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
